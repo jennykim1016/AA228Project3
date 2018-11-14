133bb47edae01a684cbd71701f267776a55b1984
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2018-11-13 21:44:30 -0800

    update code

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,7 @@
 # please fill in with the evaluation function
 
 import hyper_param
+import uniform
 
+evaluation_step = hyper_param.get_evaluation_steps()
 print(hyper_param.get_evaluation_steps())
--- uniform_baseline.py
+++ uniform_baseline.py
@@ -6,5 +6,9 @@ import random
 import hyper_param
 
 # we generate random policy for 100 times
-random_policy = []
-print(hyper_param.return_steps())
+def return_random_policy():
+    random_policy = []
+    num_step = hyper_param.get_policy_length()
+    for step in range(num_step):
+        random_policy.append(random.choice([-1, 0, 1]))
+    return random_policy
