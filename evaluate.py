2167c16a4de41e5da9f427a1c730fcfe980ff348
elzwang <ewang3@stanford.edu>
2018-11-13 21:59:52 -0800

    changed dataset

--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,12 @@
 # please fill in with the evaluation function
 
 import hyper_param
-import uniform
+import uniform_baseline
+import pandas as pd
 
-evaluation_step = hyper_param.get_evaluation_steps()
-print(hyper_param.get_evaluation_steps())
+#evaluation_step = hyper_param.get_evaluation_steps()
+#print(hyper_param.get_evaluation_steps())
+
+def generate():
+	df = pd.read_csv("ltc.csv")
+	print df[['price(USD)']].iloc[1:5]
