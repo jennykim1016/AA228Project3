663718854f23d80c566fca1413929f670d895771
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2018-12-03 22:33:00 -0800

    finish evaluation

--- evaluate_v2.py
+++ evaluate_v2.py
@@ -1,8 +1,10 @@
+from __future__ import print_function
 import numpy as np
 import random
-import hyper_param
+import hyper_param as hp
 import qlearning
 import pickle
+import evaluate
 
 def evaluate_reward(policy, test_data, random_index):
     policy_length = len(policy)
@@ -15,9 +17,6 @@ def upper_bound_of_maximum_gain(time_step, prices, horizon):
     all_prices_in_100_time_step = np.array(prices[time_step: time_step + horizon])
     closing_price = all_prices_in_100_time_step[-1]
     diff = all_prices_in_100_time_step - closing_price
-    print(horizon)
-    print(prices[time_step:time_step+horizon])
-    print(diff)
     return np.sum(np.absolute(diff))  
 
 def plotAll(t_array, upper_bound_array, reward_array):
@@ -27,7 +26,7 @@ def plotAll(t_array, upper_bound_array, reward_array):
     plt.plot(t_array, reward_array, 'go')
     plt.show()
     
-def plotAll2(t_array, upper_bound_array, reward_array):
+def plotAll2(t_array, upper_bound_array, reward_array, title):
     import pylab
     pylab.plot(t_array, upper_bound_array, 'b^', label='upper bound of utility')
     pylab.plot(t_array, reward_array, 'r^', label='utility of our policy')
@@ -35,32 +34,32 @@ def plotAll2(t_array, upper_bound_array, reward_array):
     pylab.legend(loc='upper left')
     pylab.xlabel('t')
     pylab.ylabel('Utility')
-    pylab.show()
+    pylab.savefig(title)
+
+print("here")
     
 prices = hp.get_price_history().squeeze()
 t_array = []
 upper_bound_array = []
 reward_array = []
-random_number = hyper_param.get_30_t_for_eval()
+random_number = hp.get_30_t_for_eval()
 
 for i in range(hp.get_evaluation_step()):
     random_time = random_number[i]
-    policy_length = hyper_param.get_policy_length()
+    policy_length = hp.get_policy_length()
     #policy = generate_NN_1_step_policy(random_time, prices, policy_length)
     qlearning_policy = qlearning.generate_Q_learning_policy(random_time, policy_length)
     
     t_array.append(random_time)
-    upper_bound_array.append(upper_bound_of_maximum_gain(random_time, prices, hyper_param.get_policy_length()))
+    upper_bound_array.append(upper_bound_of_maximum_gain(random_time, prices, hp.get_policy_length()))
     #reward_array.append(evaluate.evaluate_reward(policy, prices, random_time))
     reward_array.append(evaluate.evaluate_reward(qlearning_policy, prices, random_time))
     
-print(t_array)
-print(max(upper_bound_array))
-plotAll2(t_array, upper_bound_array, reward_array)
+plotAll2(t_array, upper_bound_array, reward_array, title="qlearning")
 
 ## we calculate the percentage gain
 ## change everything to the upper bound
 diff_lowerbound = -(np.array(reward_array) - np.array(upper_bound_array))
 range_ = 2*np.array(upper_bound_array)
 percent_gain = diff_lowerbound / range_
-print(np.mean(percent_gain))
+print("percentage gain:", np.mean(percent_gain))
